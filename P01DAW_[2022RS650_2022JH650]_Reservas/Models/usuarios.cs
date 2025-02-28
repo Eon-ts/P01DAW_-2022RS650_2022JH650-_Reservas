821c29296d93e60403ae961c0e87172afad448f2
Fernando <felelfer13@example.com>
2025-02-27 18:20:03 -0600

    Modelo Usuario

--- P01DAW_[2022RS650_2022JH650]_Reservas/Models/usuarios.cs
+++ P01DAW_[2022RS650_2022JH650]_Reservas/Models/usuarios.cs
@@ -1,6 +1,14 @@
-﻿namespace P01DAW__2022RS650_2022JH650__Reservas.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace P01DAW__2022RS650_2022JH650__Reservas.Models
 {
     public class usuarios
     {
+        [Key]
+        public int usuarioid { get; set; }
+        public string? nombre { get; set; }
+        public string? correo { get; set; }
+        public string? telefono { get; set; }
+        public string? contrasenia { get; set; }
+        public string? rol { get; set; }
     }
 }
